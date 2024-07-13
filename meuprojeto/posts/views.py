28f43cd630d3d9c5b49bb34b30f126c33d9f9207
BrunoBReis <brunobragancadosreis@gmail.com>
2024-07-13 20:51:52 -0300

    feat: new-post

--- meuprojeto/posts/views.py
+++ meuprojeto/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 # para o slug
 # verificador se esta tudo certo
-#from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
@@ -12,9 +13,14 @@ def posts_list_view(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
+
 def post_page_view(request, slug):
     """ Mostra a pagina do post """
-    # verificaodr se esta tudo certo 
+    # verificaodr se esta tudo certo
     # return HttpResponse(slug)
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post': post})
+
+@login_required(login_url="/users/login/")
+def post_new_view(request):
+    return render(request, 'posts/post_new.html')
